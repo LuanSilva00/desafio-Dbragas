ccffb7133b25b4a68d89f8caefbd4bd0096011c4
Luan Silva <63380413+LuanSilva00@users.noreply.github.com>
2023-09-25 23:13:38 -0300

    complete crud users without password hash

--- Dbragas/Controller/UsersController.cs
+++ Dbragas/Controller/UsersController.cs
@@ -19,11 +19,18 @@ namespace Dbragas.Controller
         [HttpGet("findAll")]
         public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
         {
-            return Ok(await _usersRepository.GetAllAsync());
+            var users = await _usersRepository.GetAllAsync();
+            if (users == null || !users.Any())
+            {
+                return NotFound("Usuários não encontrados");
+            }
+
+            return Ok(users);
         }
         [HttpPost]
         public async Task<ActionResult> AddUser(Users users)
         {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
             try
             {
                 _usersRepository.Add(users);
@@ -40,16 +47,53 @@ namespace Dbragas.Controller
             {
                 return BadRequest("Error: An error occurred while saving the user.");
             }
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error: An error occurred while processing the request.");
         }
         [HttpPatch]
         public async Task<ActionResult> PatchUser(Users users)
         {
-            try { _usersRepository.Patch(users);
-            if (await _usersRepository.SaveAllAsync())
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            try
             {
-                return Ok("User updated sucessfuly");
+                _usersRepository.Patch(users);
+                if (await _usersRepository.SaveAllAsync())
+                {
+                    return Ok("User updated sucessfuly");
+                }
             }
+            catch (Exception)
+            {
+                return BadRequest("Error: An error occurred while saving the user.");
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error: An error occurred while processing the request.");
         }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(Guid id)
+        {
+            var user = await _usersRepository.GetById(id);
+            if(user == null)
+            {
+                return NotFound("Usuário nao encontrado");
+            }
+            _usersRepository.Delete(user);
+
+            if(await _usersRepository.SaveAllAsync()) {
+                return Ok("Usuário deletado com sucesso!");
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error: An error occurred while processing the request.");
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(Guid id)
+        {
+            try
+            {
+                await _usersRepository.GetById(id);
+            }
+            catch (Exception)
+            {
+                return NotFound("Usuário não encontrado");
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error: An error occurred while processing the request.");
         }
     }
 }
